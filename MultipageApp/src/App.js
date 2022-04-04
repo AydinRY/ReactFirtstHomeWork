074e42f9267a6ee6ee2135284306490d79598089
Aydin Yusubov <aydinyusubov@Aydins-MacBook-Air.local>
2022-04-04 13:40:38 +0400

    Multipage site homework

---
+++ MultipageApp/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import Header, { Navbar } from './Components/Layouts/Navbar/Navbar';
+import Home from './Components/Pages/Home/Home';
+import Footer from './Components/Layouts/Footer/Footer';
+import About from './Components/Pages/About/About';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+function App() {
+  return(
+    <>
+    <Router>
+      <Header/>
+
+      <Switch>
+      <Route path="/" exact component={Home} />
+      <Route path="/about" exact component={About} />
+      </Switch>
+      <Footer/>
+      </Router>
+    </>
+  )
+}
+
+export default App;
